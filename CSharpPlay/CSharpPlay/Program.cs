7e46cc5f292f28f408d0ee9d663a110da4d2a0fc
silarr <silar.profile@gmail.com>
2015-10-18 16:12:12 -0700

    Bind example

---
+++ CSharpPlay/CSharpPlay/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPlay
+{
+    public class BaseNancyModule
+    {
+        // Just to mock Nancy's behaviour
+        public Func<string, string> Get;
+
+        public string Bind<T>(Func<string> getResponseFunc)
+        {
+            this.Bind<T>();
+            return getResponseFunc();
+        }
+
+        public void Bind<T>()
+        {
+            // Implemented by Nancy
+        }
+    }
+
+    public class SomeNancyModule : BaseNancyModule
+    {
+        public void Request()
+        {
+            this.Get = _ => this.Bind<MyModel>(() => this.GetResponse());
+        }
+
+        public string GetResponse()
+        {
+           return "DO something";
+        }
+    }
+
+    public class MyModel
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new SomeNancyModule();
+            t.Request();
+            var r = t.Get("some_url");
+        }
+    }
+}
